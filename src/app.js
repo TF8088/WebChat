db258c487dceb6b4c92b8b9e7e78bcd410225c66
TF8088 <tiagofarinha03@outlook.com>
2023-07-13 12:32:24 +0100

    Pug Engine Added, Simple Route Added

--- src/app.js
+++ src/app.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const path = require('path');
 const app = express();
 
+// dotenv call requeried 
 require('dotenv').config()
-
+// app set pug engine
+app.set('view engine', 'pug')
+// app set views direct
+app.set('views', [
+  path.join(__dirname, './app/frontend'),
+]);
+// app set static route direct
+app.use('/static', express.static(path.join(__dirname, './app/frontend/static')));
+// simple get route /
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render('home', { title: 'WebChat', message: 'Welcome to WebChat' })
 });
-
+// server start
 app.listen(process.env.PORT, () => {
-    console.log(`Weatherpt Project is running on: http://localhost:` + process.env.PORT);
+    console.log(`WebChat: http://localhost:` + process.env.PORT);
 });
