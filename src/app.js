2969e5359997f86090a1cb1665989e4e6c85b236
TF8088 <tiagofarinha03@outlook.com>
2023-07-13 12:57:52 +0100

    WebSockets Added, Backend

--- src/app.js
+++ src/app.js
@@ -2,21 +2,38 @@ const express = require("express");
 const path = require('path');
 const app = express();
 
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
 // dotenv call requeried 
 require('dotenv').config()
+
 // app set pug engine
 app.set('view engine', 'pug')
+
 // app set views direct
 app.set('views', [
   path.join(__dirname, './app/frontend'),
 ]);
+
 // app set static route direct
 app.use('/static', express.static(path.join(__dirname, './app/frontend/static')));
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
 // simple get route /
 app.get("/", (req, res) => {
   res.render('home', { title: 'WebChat', message: 'Welcome to WebChat' })
 });
+
 // server start
-app.listen(process.env.PORT, () => {
-    console.log(`WebChat: http://localhost:` + process.env.PORT);
+server.listen(process.env.PORT, () => {
+  console.log(`WebChat: http://localhost:` + process.env.PORT);
 });
